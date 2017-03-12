2de61ae95b4ecdb5e79ff08cc2d4a595b446205b
Jow Christophe <christophe.jow@student.ecp.fr>
2017-03-12 14:43:30 +0100

    Resize face images

--- Computer_Vision/plot_images.py
+++ Computer_Vision/plot_images.py
@@ -6,19 +6,8 @@ import glob
 
 
 if __name__ == '__main__':
-    dir = "./photos/00/*.jpg"
-    file = glob.glob(dir)[0]
-    img = cv2.imread(file)
-    plt.imshow(img)
-    plt.show()
-
     data = np.load('my_array.npy')
-    img = data[0][:][:][:]
-    plt.imshow(img)
-    plt.show()
-
-    """data = np.load('my_array.npy')
     for i in range(0, 10):
-        img = data[0][:][:][:]
+        img = data[i][:][:][:]
         plt.imshow(img)
-        plt.show()"""
+        plt.show()
