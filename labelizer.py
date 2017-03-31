a9b804de0975f1f85e6c18603aa7812d66404568
kasimansour <kasramansouri94@gmail.com>
2017-03-31 18:14:10 +0200

    nothing

--- labelizer.py
+++ labelizer.py
@@ -2,9 +2,10 @@ import re
 import os
 import numpy as np
 
+DIRPATH = '/home/kasi/Documents/Deep_Learning/wiki/'
+
+
 def find_age(str):
-
-
     regex = r"_([0-9]+).+_([0-9]+)"
     matches = re.search(regex, str)
     if matches:
@@ -37,32 +38,40 @@ def turn_age_into_vector(age):
         pass
 
 
-def labelize_images():
+def labelize_images(n):
+    folders = range(n)
     l = []
-    dirpath = '/home/kasi/Documents/Deep_Learning/wiki/'
-    for folder in os.listdir(dirpath):
-        if folder != "wiki.mat":
-            for image in os.listdir('/home/kasi/Documents/Deep_Learning/wiki/' + folder):
-                image_age = find_age(image)
-                # print('age on photo {}'.format(image_age))
-                if image_age > 0:
-                    l.append(image_age)
+    for folder in folders:
+        for image in os.listdir('/home/kasi/Documents/Deep_Learning/wiki/' + str(folder).zfill(2)):
+            image_age = find_age(image)
+            # print('age on photo {}'.format(image_age))
+            if image_age > 0:
+                l.append(image_age)
     vector_length = len(l)
     k = 0
     y = np.zeros((vector_length, 7))
-    # print(y)
-    for folder in os.listdir(dirpath):
-        for image in os.listdir('/home/kasi/Documents/Deep_Learning/wiki/' + folder):
+    # y = np.zeros((575, 7))
+    counter = 0
+    for folder in folders:
+        print(folder)
+        for image in os.listdir('/home/kasi/Documents/Deep_Learning/wiki/' + str(folder).zfill(2)):
             image_age = find_age(image)
             if image_age > 0:
                 y[k] = turn_age_into_vector(image_age)
+                counter += 1
                 k += 1
+    print("number of pictures stored in training set: {}".format(counter))
     return y
 
 
-def save_array():
-    labels = labelize_images()
+def save_array(n):
+    labels = labelize_images(n)
     np.save('wiki_labels.npy', labels)
 
 
-save_array()
+if __name__ == "__main__":
+    number_of_folders = 20
+    save_array(number_of_folders)
+
+# f= np.load('wiki_labels.npy')
+# print(f)
